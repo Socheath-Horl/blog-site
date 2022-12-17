b6fedea0477f016115362c4ef0da7eebb9d727a6
Horl-Socheath <horl.socheath@gmail.com>
2022-12-17 20:20:12 +0700

    seo friendly

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(
         to=User,
         on_delete=models.CASCADE,
