68005633fc1969f8d0a03e0a1c1d44ca3e61dc7c
Horl-Socheath <horl.socheath@gmail.com>
2022-12-17 21:08:20 +0700

    class-based view

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,32 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(
-        request=request, 
-        template_name='blog/post/list.xhtml',
-        context={'posts': posts}
-    )
+class PostListView(ListView):
+    """Alternative post list view"""
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.xhtml'
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(
+#         request=request, 
+#         template_name='blog/post/list.xhtml',
+#         context={'posts': posts}
+#     )
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
