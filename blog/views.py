cdb6463beef5d565928817422c8bfecd1f74312f
Horl-Socheath <horl.socheath@gmail.com>
2022-12-17 20:46:50 +0700

    paginations

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request=request, 
         template_name='blog/post/list.xhtml',
