b248063ff11d8a679e5e684cae98cb2375543261
Horl-Socheath <horl.socheath@gmail.com>
2022-12-17 19:49:17 +0700

    get_object_or_404

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class PublishedManager(models.Manager):
 
     def get_queryset(self):
         return super().get_queryset() \
-                        .filter(status=Post.status.PUBLISHED)
+                        .filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -14,7 +14,11 @@ def post_list(request):
 
 def post_detail(request, id):
     try:
-        post = Post.published.get(id=id)
+        post = get_object_or_404(
+            Post,
+            id=id,
+            status=Post.Status.PUBLISHED
+        )
     except Post.DoesNotExist:
         raise Http404("No Post found.")
     
