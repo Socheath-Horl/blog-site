6f152e006cd0a2cafc27aeca52506c5735ad6a5c
Horl-Socheath <horl.socheath@gmail.com>
2022-12-18 11:04:58 +0700

    similar post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -42,10 +43,22 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+     
     return render(
         request=request,
         template_name='blog/post/detail.xhtml',
-        context={'post': post, 'comments': comments, 'form': form}
+        context={
+            'post': post,
+            'comments': comments,
+            'form': form,
+            'similar_posts': similar_posts
+        }
     )
 
 def post_share(request, post_id):
