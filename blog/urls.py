c48440c465babcc05a9fa4667278609bbd1f604f
Horl-Socheath <horl.socheath@gmail.com>
2022-12-18 12:51:51 +0700

    rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostFeed(), name='post_feed'),
 ]
