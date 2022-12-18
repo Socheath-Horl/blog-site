6a68e06cae1c1cb154892c6b41596084dfe1bb39
Horl-Socheath <horl.socheath@gmail.com>
2022-12-18 12:33:56 +0700

    sitemap for seo

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
